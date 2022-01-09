04b37036ddd02cac7599ea2a78cbab2d9a3ba439
rosenbahnev <rbb86@abv.bg>
2022-01-09 19:12:17 +0200

    Remove MogoDB code.

--- handlers/home/index.js
+++ handlers/home/index.js
@@ -1,10 +1,7 @@
-// const Tripps = require('../trips')
-
 module.exports = {
     get: {
         home(req, res, next) {
             res.render('./home/home.hbs', {
-                // isLoggedIn: req.user ? true : false,
                 isLoggedIn: res.locals.user ? true : false,
                 username: res.locals.user ? res.locals.user : null
             })
--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-require('./config/database')().then(()=>{
-    const config = require('./config/config');
-    const app = require('express')()
-    const appString = `SErver is ready, listening on port: ${config.port}`
+const config = require('./config/config');
+const app = require('express')()
+const appString = `Server is ready, listening on port: ${config.port}`
 
-    require('./config/express')(app)
-    require('./config/routes')(app)
+require('./config/express')(app)
+require('./config/routes')(app)
 
-    app.listen(config.port, console.log(appString))
-})
+app.listen(config.port, console.log(appString))
